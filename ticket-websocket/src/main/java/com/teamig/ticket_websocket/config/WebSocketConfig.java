2eefa0550f99c7c0f4450c5af6e5fdbd23dbb47d
kris1013 <kristen.kelly-smith@ada.ac.uk>
2022-04-06 14:28:29 +0100

    newer version of websocket

--- ticket-websocket/src/main/java/com/teamig/ticket_websocket/config/WebSocketConfig.java
+++ ticket-websocket/src/main/java/com/teamig/ticket_websocket/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.teamig.ticket_websocket;
+package com.teamig.ticket_websocket.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -12,13 +12,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/database", "/client");
+        config.setUserDestinationPrefix("/client");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+        registry.addEndpoint("/ticket").setAllowedOrigins("*").withSockJS();
     }
 
 }
